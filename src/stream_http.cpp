d695bf97959b1f6e885479a9bba94adc62589873
5SXZ734 <shulgaaa@gmail.com>
2026-05-23 12:42:55 -0700

    Fix Windows build for HTTP stream module and socket linking

--- src/stream_http.cpp
+++ src/stream_http.cpp
@@ -1,11 +1,36 @@
 #include "stream_http.h"
 
+#ifdef _WIN32
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#else
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#endif
 
 #include <cstring>
+
+namespace {
+#ifdef _WIN32
+using SocketHandle = SOCKET;
+constexpr SocketHandle kInvalidSocket = INVALID_SOCKET;
+void closeSocket(SocketHandle s) {
+    if (s != INVALID_SOCKET) {
+        closesocket(s);
+    }
+}
+#else
+using SocketHandle = int;
+constexpr SocketHandle kInvalidSocket = -1;
+void closeSocket(SocketHandle s) {
+    if (s >= 0) {
+        close(s);
+    }
+}
+#endif
+}
 
 HttpStreamServer::HttpStreamServer(std::string bindAddress, int port, MetricsFn metricsFn)
     : bindAddress_(std::move(bindAddress)), port_(port), metricsFn_(std::move(metricsFn)) {}
@@ -25,8 +50,12 @@ void HttpStreamServer::stop() {
         return;
     }
     if (serverFd_ >= 0) {
+#ifdef _WIN32
+        shutdown(static_cast<SOCKET>(serverFd_), SD_BOTH);
+#else
         shutdown(serverFd_, SHUT_RDWR);
-        close(serverFd_);
+#endif
+        closeSocket(static_cast<SocketHandle>(serverFd_));
         serverFd_ = -1;
     }
     if (worker_.joinable()) {
@@ -35,12 +64,26 @@ void HttpStreamServer::stop() {
 }
 
 void HttpStreamServer::run() {
-    serverFd_ = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverFd_ < 0) {
+#ifdef _WIN32
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        running_.store(false);
         return;
     }
+#endif
+
+    SocketHandle serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == kInvalidSocket) {
+#ifdef _WIN32
+        WSACleanup();
+#endif
+        running_.store(false);
+        return;
+    }
+    serverFd_ = static_cast<int>(serverSocket);
+
     int opt = 1;
-    setsockopt(serverFd_, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, reinterpret_cast<const char*>(&opt), sizeof(opt));
 
     sockaddr_in addr{};
     addr.sin_family = AF_INET;
@@ -51,21 +94,29 @@ void HttpStreamServer::run() {
         inet_pton(AF_INET, bindAddress_.c_str(), &addr.sin_addr);
     }
 
-    if (bind(serverFd_, reinterpret_cast<sockaddr*>(&addr), sizeof(addr)) < 0) {
-        return;
-    }
-    if (listen(serverFd_, 8) < 0) {
+    if (bind(serverSocket, reinterpret_cast<sockaddr*>(&addr), sizeof(addr)) < 0 ||
+        listen(serverSocket, 8) < 0) {
+        closeSocket(serverSocket);
+#ifdef _WIN32
+        WSACleanup();
+#endif
+        running_.store(false);
+        serverFd_ = -1;
         return;
     }
 
     while (running_.load()) {
-        int client = accept(serverFd_, nullptr, nullptr);
-        if (client < 0) {
+        SocketHandle client = accept(serverSocket, nullptr, nullptr);
+        if (client == kInvalidSocket) {
             continue;
         }
 
         char buffer[2048] = {0};
+#ifdef _WIN32
+        const int n = recv(client, buffer, static_cast<int>(sizeof(buffer) - 1), 0);
+#else
         const ssize_t n = read(client, buffer, sizeof(buffer) - 1);
+#endif
         std::string req = n > 0 ? std::string(buffer, static_cast<std::size_t>(n)) : "";
         std::string body;
         std::string status = "200 OK";
@@ -81,7 +132,17 @@ void HttpStreamServer::run() {
 
         const std::string resp = "HTTP/1.1 " + status + "\r\nContent-Type: text/plain\r\nContent-Length: " +
                                  std::to_string(body.size()) + "\r\nConnection: close\r\n\r\n" + body;
+#ifdef _WIN32
+        send(client, resp.c_str(), static_cast<int>(resp.size()), 0);
+#else
         send(client, resp.c_str(), resp.size(), 0);
-        close(client);
+#endif
+        closeSocket(client);
     }
+
+    closeSocket(serverSocket);
+    serverFd_ = -1;
+#ifdef _WIN32
+    WSACleanup();
+#endif
 }
